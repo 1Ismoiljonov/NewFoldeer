2116208d909cacf6630c5e1c45ac4f274f6ea107
1Ismoiljonov <samandarismoiljonov9@gmail.com>
2023-04-11 00:32:14 +0500

    first lesson

---
+++ lesson-1/node.js
@@ -0,0 +1,11 @@
+// let num = 5;
+// console.log(num++);  => bunda natijani ekranga chiqara olmaydi sabab qo'shgandan oldin console ga chiqarib keyin qoshyabmiz 
+// num++; => shu holat bo'lib keyin console.log qiladigan bo'lsak aniq natija chiqadi 
+
+// yozgan codimizni type ni tekshirish uchun (typeof)  funksiyasidan foydalaniladi.
+//masalan 
+//let num = 5;
+//console.log(typeof num) => shunda uni type ni ko'rsatib beradi
+// null ning typeofi object ? 
+// console.log(num); 
+console.log(false + 1); // bunda javob "0" chiqadi
