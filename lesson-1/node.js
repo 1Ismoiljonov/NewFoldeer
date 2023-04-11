c82648a3175f2be35a0dfee05e6348051d4850f7
1Ismoiljonov <samandarismoiljonov9@gmail.com>
2023-04-11 11:45:46 +0500

    first lesson

--- lesson-1/node.js
+++ lesson-1/node.js
@@ -7,5 +7,55 @@
 //let num = 5;
 //console.log(typeof num) => shunda uni type ni ko'rsatib beradi
 // null ning typeofi object ? 
+//NaN ning data typesi "Number"
 // console.log(num); 
 // console.log(false + 1); // bunda javob "0" chiqadi
+// NaN => Not a Number
+// console.log("Samandar" - "Iqboljon"); => NaN . 
+
+// console.log(5 > 3);
+
+// console.log(undefined == null);   true
+// console.log(undefined === null);  false 
+
+// console.log(5 == "5");
+// console.log(5 === "5"); // 3 talik tenglikda yozilgan raqamni typeiga qaraladi
+
+// let num1 = 5
+// var num2 = 5 
+// const num3 = 5 
+
+// num1 = 7
+// console.log(num1);
+
+// let num = 5
+// num++ 
+// console.log(num++);
+// console.log(++num);
+// console.log(num);
+
+// if else
+
+// let num = 6
+// if (num > 20) {
+//     console.log("20 dan katta");
+// } else if (num == 20) {
+//     console.log("20 ga teng");
+// } else if (num < 20) {
+//     console.log("20 dan kechik");
+// }
+
+// let result ;
+// if (a + b < 4){
+//     result = "below"
+// }else {
+//     result =  "over"
+// }
+// console.log(result);
+
+// console.log(5 > 3 ? "true" : "false");  // javob true 
+
+// console.log(false || false || false || true);  // OR
+// console.log(0 + 0 + 0 + 0 + 1);
+
+// console.log(false && false && false && true); // AND
