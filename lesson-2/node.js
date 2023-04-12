672a75a165966cf059dafa2ce5966614a83247aa
1Ismoiljonov <samandarismoiljonov9@gmail.com>
2023-04-13 00:37:33 +0500

    lesson-2

--- lesson-2/node.js
+++ lesson-2/node.js
@@ -1,10 +1,100 @@
 // console.log("Samandar Ismoiljonov");
-let result ;
-let a = 1;
-let b = 1
-if (a+b < 4 ){
-    result = "below"
-}else {
-    result = "over"
+// let result ;
+// let a = 1;
+// let b = 1
+// if (a+b < 4 ){
+//     result = "below"
+// }else {
+//     result = "over"
+// }
+// console.log(result);  parseInt va parsFloatni vazig=fasi stringni number tipiga o'tkazish
+    // console.log(Boolean("web"));
+
+
+// false ni ifodalaydigan qiymatlar => 0 , false, null, NaN, undefined, ""  .
+
+
+// console.log("Webbrain" || false);
+// console.log("Webbrain" && false);
+
+
+//  ??  nullish operators bunda null va undefined ni qidiradi
+
+//    SWITCH CASE
+
+// let havo = "3"
+// let havo = "issiq"
+// let havo = "sovuq"
+// switch(havo){
+// case "qor" : console.log("bugun qor"); 
+// break;
+// case "issiq" : console.log("bugun havo issiq") 
+// break;
+// case "sovuq" : console.log("bugun havo sovuq");
+// break;
+// default : console.log("no data")
+// }
+
+// let num = 100;
+// for (let i = 0; index < num.length; i++) {
+//     if((num[i])/ (num[i]) == 1) {
+//         console.log("bu son tub son ");
+//     }else{
+//         console.log("tub son emas");
+//     }
+// }
+
+// let n = 100;
+
+// c = 0;
+
+// for (let j = 2; j <= n; j++) {
+//     for (let i = 1; i <= j; i++) {
+//         if (j % i === 0) {
+//             c++;
+//         }
+//     }
+//     if (c == 2) {
+//         console.log(`tub son ${j}`);
+//     }
+//     c = 0;
+// }
+
+let names = "213548643153"
+let result = 0
+for(let i = 0; i <= names.length; i++ ) {
+    if(names[i] == "1") {
+        console.log("bir");
+        result++
+    }else if (names[i] == "2") {
+        console.log("ikki");
+        result++
+    }else if (names[i] == "3"){
+        console.log("uch");
+        result++
+    }else if (names[i] == "4"){
+        console.log("to'rt");
+        result++
+    }else if (names[i] == "5"){
+        console.log("besh");
+        result++
+    }else if (names[i] == "6"){
+        console.log("olti");
+        result++
+    }else if (names[i] == "7"){
+        console.log("yetti");
+        result++
+    }else if (names[i] == "8"){
+        console.log("sakkiz");
+        result++
+    }else if (names[i] == "9"){
+        console.log("to'qqiz");
+        result++
+    }
 }
-console.log(result);
+
+
+
+
+
+
